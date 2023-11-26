1f435ca8eb13253e8d21c7580beb7dd708c918b0
Anthony P <147562280+AnthonyPraser@users.noreply.github.com>
2023-11-26 07:06:36 -0500

    Adding to my GitHub repo

--- class14/AnthonyPrasertsinh_Project1.py
+++ class14/AnthonyPrasertsinh_Project1.py
@@ -1,10 +1,113 @@
 # How should I start? Telling the user the username and password requirments
 
-username_requirments = "Username Requirements: It must start with a lowercase letter and only contain letters, numbers, underscores, and not be in a list of taken usernames."
-print(username_requirments)
+# username_requirments = "Username Requirements: It must start with a lowercase letter and only contain letters, numbers, underscores, and not be in a list of taken usernames."
+# taken_usernames = ['admin', 'admin123', 'root']
+# print(username_requirments)
 
-password_requirments = "Password Requirements: At least 8 characters long, contains at least one uppercase letter, contains at least one lowercase letter, contains at least one digit, contains at least one of these characters: !, ?, @, #, $, ^, &, *, _, -, and doesn't contain any spaces."
-print(password_requirments)
+# password_requirments = "Password Requirements: At least 8 characters long, contains at least one uppercase letter, contains at least one lowercase letter, contains at least one digit, contains at least one of these characters: !, ?, @, #, $, ^, &, *, _, -, and doesn't contain any spaces."
+# print(password_requirments)
+
+
+# Trying to figure out the username requirments part
+
+# user_input_username = input("What is your username: ")
+
+# start_lowercase_letter = len(user_input_username[0]) == user_input_username.isalpha() and user_input_username.islower()
+
+# contain_letters = (user_input_username.isalpha())
+
+# contain_numbers = (user_input_username.isdigit())
+
+# contain_underscores = (user_input_username == "_")
+
+# taken_usernames = ['admin', 'admin123', 'root']
+
+# username requirments in a list
+
+# user_requirment_list = [start_lowercase_letter, contain_letters, contain_numbers, contain_underscores,]
+
+
+# The user must be continuously prompted for input
+
+user_input_username =  input("What is your username: ") 
+
+
+start_lowercase_letter = len(user_input_username[0]) == user_input_username.isalpha() and user_input_username.islower()
+contain_numbers = (user_input_username.isdigit())
+contain_underscores = (user_input_username == "_")
+taken_usernames = ['admin', 'admin123', 'root']
+contain_letters = (user_input_username.isalpha())
+
+while True:
+    
+
+
+
+        
+    
+   
+
+
+    
+   
+        
+
+
+ 
+
+   
+
+
+
+
+ 
+
+        
+   
+  
+        
+
+   
+        
+    
+    
+    
+
+        
+    
+
+
+   
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+  
+        
+        
+
+    
+    
+    
+
+    
+    
+
+
 
 
 
