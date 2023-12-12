b5c36b57a6e8c4ae1c652801d127f3fa30f0f67d
Anthony P <147562280+AnthonyPraser@users.noreply.github.com>
2023-12-11 20:53:44 -0500

    Adding to GitHub repo

---
+++ class19/anthonyprasertsinh_assignment6.py
@@ -0,0 +1,99 @@
+'''
+Assignment 6
+Write a simple function that returns a person's net worth. Here are your requirements:
+Docstring which includes what function does and information on your parameters (brief)
+Function name - net_worth
+parameters - assets, liabilities
+Must contain type hinting for the parameters as well as what the function will be returning
+Call the function in a print statement with needed arguments
+'''
+
+def net_worth(assets: float, liabilities: float) -> float:
+    '''Function for figuring out someones net worth through their 
+    assets and liabilities'''
+    
+    return assets - liabilities
+
+print(net_worth(40.000, 10.000))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
