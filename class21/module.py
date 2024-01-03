1e6ea894a7c2c24320219d33cc047ec6986b7908
Anthony P <147562280+AnthonyPraser@users.noreply.github.com>
2024-01-03 18:28:06 -0500

    Adding to GitHub repo

--- class21/module.py
+++ class21/module.py
@@ -3,53 +3,56 @@ import pandas as pd
 
 class BankAccount:
     
+    
 
     # Init Function
-    def __init__():
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
         pass
 
 
-    # __str__
-    def __str__():
-        pass
+    # # __str__
+    # def __str__(self):
+    #     pass
 
 
-    # deposit_funds()
-    def deposit_funds():
-        pass
+    # # deposit_funds()
+    # def deposit_funds(self):
+    #     pass
 
-    # withdraw_funds()
-    def withdraw_funds():
-        pass
-
-
-    # get_balance()
-    def get_balance():
-        pass
+    # # withdraw_funds()
+    # def withdraw_funds(self):
+    #     pass
 
 
-    # get_transaction()
-    def get_transaction():
-        pass
+    # # get_balance()
+    # def get_balance(self):
+    #     pass
 
 
-    # transaction_count()
-    def get_transaction():
-        pass
+    # # get_transaction()
+    # def get_transaction(self):
+    #     pass
 
 
-    # transaction_history()
-    def transaction_history():
-        pass
+    # # transaction_count()
+    # def get_transaction(self):
+    #     pass
 
 
-    # save_transaction()
-    def save_transaction():
-        pass
-
-  
+    # # transaction_history()
+    # def transaction_history(self):
+    #     pass
 
 
+    # # save_transaction()
+    # def save_transaction(self):
+    #     pass
+
+my_bank_account = BankAccount("Anthony", 4000)
+
+print(my_bank_account)
 
 
 
