ba49dce6c8d35b494549d5a0a38bd6e1ea13d5c0
Anthony P <147562280+AnthonyPraser@users.noreply.github.com>
2024-01-03 19:57:19 -0500

    Adding to GitHub repo

--- class21/module.py
+++ class21/module.py
@@ -19,28 +19,54 @@ class BankAccount:
 
     # deposit_funds()
     def deposit_funds(self, amount):
-        t = {'owner': self.owner, 'type': 'deposit', 'amount': {amount}}
+        t = {'owner': self.owner, 'type': 'deposit', 'amount': amount}
         self.transactions.append(t)
         return self.transactions
         
 
     # withdraw_funds()
     def withdraw_funds(self, amount):
-        t = {'owner': self.owner, 'type': 'withdrawal', 'amount': {amount}}
-        self.transactions.append(t)
-        return self.transactions
 
+        deposits = sum(s['amount'] for s in self.transactions if s['type'] == 'deposit')
+        total_balance = deposits + self.balance
+        
+        if total_balance < amount:
+            print("Insufficient Funds")
+        else:
+            t = {'owner': self.owner, 'type': 'withdraws', 'amount': amount}
+            self.transactions.append(t)
+            return self.transactions
+
+       
+
+
+
+
+
+    
+
+      
+
+
+    # get_balance()
+    def get_balance(self):
+        deposits = sum(s['amount'] for s in self.transactions if s['type'] == 'deposit')
+        withdraws = sum(s['amount'] for s in self.transactions if s['type'] == 'withdraws')
+        balance = self.balance + (deposits - withdraws)
+        print(f'Your account balance is ${balance: 2f}')
+
+        
+       
         
 
 
-    # # get_balance()
-    # def get_balance(self):
-    #     pass
+    # get_transaction()
+    def get_transaction(self):
+        print(self.transactions)
 
-
-    # # get_transaction()
-    # def get_transaction(self):
-    #     pass
+    def get_transaction_count(self):
+        print(len(self.transactions))
+        
 
 
     # # transaction_count()
@@ -68,11 +94,21 @@ anthony_account = BankAccount("Anthony", 4000)
 anthony_account.deposit_funds(50)
 anthony_account.deposit_funds(200)
 anthony_account.deposit_funds(300)
+anthony_account.deposit_funds(300)
 
 # Testing withdraw
+anthony_account.withdraw_funds(400)
+anthony_account.withdraw_funds(200)
 anthony_account.withdraw_funds(100)
-anthony_account.withdraw_funds(20)
-anthony_account.withdraw_funds(150)
+
+# Testing get balance
+anthony_account.get_balance()
+
+# Testing get transactions
+anthony_account.get_transaction()
+
+# Testing transaction count
+anthony_account.get_transaction_count()
 
 
 
